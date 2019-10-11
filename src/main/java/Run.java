b9a16f019007b03858688d6e78dd6afbd04f28f5
Chadha <shatha.degachi@gmail.com>
2019-10-11 13:13:48 +0100

    Started refactoring project - extracted functionality into methods out of the main method

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,8 +1,9 @@
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.nio.file.*;
-import java.util.Arrays;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.logging.Logger;
 
 public class Run {
@@ -11,26 +12,29 @@ public class Run {
     public static void main(String[] args) {
         {
             try {
-                Path coverImagePath = Paths.get("src/main/java/images/butterfly.bmp");
-                Path filePath = Paths.get("src/main/java/textfiles/example1.txt");
 
-                InputStream coverInputStream = Files.newInputStream(coverImagePath, StandardOpenOption.CREATE_NEW);
-                InputStream fileInputStream = Files.newInputStream(filePath, StandardOpenOption.CREATE_NEW);
+                byte[] coverImagesBytes = processInput("src/main/java/images/butterfly.bmp");
+                byte[] fileBytes = processInput("src/main/java/textfiles/example1.txt");
 
-                byte[] coverImagesBytes = coverInputStream.readAllBytes();
-                byte[] fileBytes = fileInputStream.readAllBytes();
-                byte aByte  = fileBytes[0];
-                // testing byte conversion
+                encodeFile(coverImagesBytes, fileBytes);
 
-                log.info("a byte " +  aByte + " a byte as unsigned int is: " + Byte.toUnsignedInt(aByte));
-
-                //start at 54 to skip header of image
-                for (int x = 54; x < coverImagesBytes.length; x = x + 8 ){
-
-                }
             } catch (IOException e) {
-                log.warning("File i/o error, exception " +  e.toString());
+                log.warning("File I/O exception: " + e.toString());
             }
+        }
+    }
+
+    private static byte[] processInput(String path) throws IOException {
+        Path inputPath = Paths.get(path);
+        InputStream inputStream = Files.newInputStream(inputPath, StandardOpenOption.CREATE_NEW);
+
+        return inputStream.readAllBytes();
+    }
+
+    private static void encodeFile(byte[] coverImage, byte[] input){
+        //start at 54 to skip header of image
+        for (int x = 54; x < coverImage.length; x = x + 8) {
+            //TODO
         }
     }
 }

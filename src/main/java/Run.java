73a4da41ac9adf7ced1e354ec36eae86d67c973e
Chadha <shatha.degachi@gmail.com>
2019-10-14 20:07:44 +0100

    value out of range byte parser

--- src/main/java/Run.java
+++ src/main/java/Run.java
@@ -1,37 +1,38 @@
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.BitSet;
-import java.util.List;
 import java.util.logging.Logger;
 
 public class Run {
     private final static Logger log = Logger.getLogger(Run.class.getName());
 
     public static void main(String[] args) {
-       encodeFile("src/main/java/images/butterfly.bmp", "src/main/java/textfiles/example1.txt");
+        encodeFile("src/main/java/images/butterfly.bmp", "src/main/java/textfiles/example1.txt");
     }
 
-    private static void decodeFile(String coverImagePath){
-        byte[] coverImageBytes =  processInput(coverImagePath);
+    private static void decodeFile(String coverImagePath) {
+        byte[] coverImageBytes = processInput(coverImagePath);
         try {
             OutputStream out = new FileOutputStream("output path");
             out.write(decodeFile(coverImageBytes));
         } catch (IOException e) {
-            log.warning("IO exception "  + e );
+            log.warning("IO exception " + e);
         }
     }
 
-    private static byte[] decodeFile(byte[] coverImageBytes){
+    private static byte[] decodeFile(byte[] coverImageBytes) {
         //TODO
-        return new byte[] {};
+        return new byte[]{};
     }
 
-    private static void encodeFile(String coverImagePath, String inputPath){
+    private static void encodeFile(String coverImagePath, String inputPath) {
         //TODO throw error if input is too big to be hidden in cover image
         byte[] coverImageBytes = processInput(coverImagePath);
         byte[] fileBytes = processInput(inputPath);
@@ -44,10 +45,10 @@ public class Run {
         byte[] header = Arrays.copyOfRange(coverImage, 0, 53);
         byte[] image = Arrays.copyOfRange(coverImage, 54, coverImage.length - 1);
 
-        log.info("Example byte of image[0]: " +  image[0]);
+        log.info("Example byte of image[0]: " + image[0]);
         log.info("Example byte of input [0]: " + input[0]);
 
-        //convert Bytes[] to bits
+        //convert image bytes[] to bits
         ArrayList<String> imageBits = new ArrayList();
         for (int x = 0; x < image.length; x++) {
             imageBits.add(Integer.toBinaryString(image[x] & 0xFF));
@@ -55,46 +56,56 @@ public class Run {
 
         log.info("Example bit of image[0] " + imageBits.get(0));
 
+        //convert input bytes[] to bits
         ArrayList<String> inputBits = new ArrayList();
         for (int x = 0; x < input.length; x++) {
             String inputBit = Integer.toBinaryString(input[x] & 0xFF);
-            if(inputBit.length() < 8 ){
+            //pad input bits with zeros to produce fixed length bytes to encode
+            if (inputBit.length() < 8) {
                 int padding = 8 - inputBit.length();
-                for(int i = 0; i < padding; i++){
+                for (int i = 0; i < padding; i++) {
                     inputBit = "0" + inputBit;
                 }
             }
-            inputBits.add(Integer.toBinaryString(input[x] & 0xFF));
+            inputBits.add(inputBit);
         }
         log.info("Example bit of input " + inputBits.get(0));
 
-        int inputPosition = 0;
-        for (int x = 0; x < imageBits.size(); x++){
-            String newValue = imageBits.get(x).substring(0, imageBits.get(x).length() -1) +  inputBits.get(inputPosition));
+        int inputBytePosition = 0;
+        int inputBitPosition = 0;
+        for (int x = 0; x < imageBits.size(); x++) {
+            String newValue = imageBits.get(x).substring(0, imageBits.get(x).length() - 1) + inputBits.get(inputBytePosition).substring(inputBitPosition, inputBitPosition + 1);
             imageBits.set(x, newValue);
-            inputPosition++;
-            if (inputPosition == input.length){
+
+            inputBitPosition++;
+
+            if(inputBitPosition == 7){
+               inputBytePosition++;
+               inputBitPosition = 0;
+            }
+
+            if (inputBytePosition == input.length) {
                 break;
             }
         }
 
-        log.info("Example encoded bit of image[0] " + imageBits.get(0));
+        log.info("Example encoded bit of image [0] " + imageBits.get(0));
 
         //convert bits to Bytes[]
         ArrayList<Byte> newImageBytes = new ArrayList<>();
-        inputBits.forEach( b -> newImageBytes.add(Byte.parseByte(b, 2)));
+        imageBits.forEach(b -> newImageBytes.add(Byte.parseByte(b, 2)));
 
-        log.info("Example encoded byte of image[0] "+ newImageBytes.get(0));
+        log.info("Example encoded byte of image[0] " + newImageBytes.get(0));
 
         renderOutputImage(newImageBytes);
     }
 
-    private static void renderOutputImage(ArrayList<Byte> newImageBytes){
+    private static void renderOutputImage(ArrayList<Byte> newImageBytes) {
         //TODO
     }
 
 
-    private static byte[] processInput(String path)  {
+    private static byte[] processInput(String path) {
         Path inputPath = Paths.get(path);
         InputStream inputStream = InputStream.nullInputStream();
         try {

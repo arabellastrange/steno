0dd4ea38ab8a1e3fbfbd5c8c4d210ada89be76d8
Ciaran Service (Student) <xsb15121@ds.strath.ac.uk>
2019-10-15 11:31:13 +0100

    working image rendering

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 407-steganography
 ## Instructions for running this programme
+
+info on file headers: http://www.ece.ualberta.ca/~elliott/ee552/studentAppNotes/2003_w/misc/bmp_file_format/bmp_file_format.htm
--- src/main/java/BitSetStego.java
+++ src/main/java/BitSetStego.java
@@ -1,7 +1,6 @@
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,11 +9,12 @@ import java.util.Arrays;
 import java.util.BitSet;
 import java.util.logging.Logger;
 
+
 public class BitSetStego {
     private static final Logger log = Logger.getLogger(BitSetStego.class.getName());
 
     public static void main(String[] args) {
-        encodeFile("src/main/java/images/butterfly.bmp", "src/main/java/textfiles/example1.txt");
+        encodeFile("src/main/java/images/all_white_test.bmp", "src/main/java/textfiles/example1.txt");
     }
 
     private static void decodeFile(String coverImagePath) throws IOException {
@@ -45,8 +45,9 @@ public class BitSetStego {
 
     private static void encodeFile(byte[] coverImage, byte[] input) {
         // start at 54 to skip header of image, split into header and image
-        byte[] image = Arrays.copyOfRange(coverImage, 54, coverImage.length - 1);
-
+        byte[] image = Arrays.copyOfRange(coverImage, 54, coverImage.length);
+        byte[] header = Arrays.copyOfRange(coverImage, 0, 54);
+        log.info("Original image header " + header[0]);
         log.info("Example byte of image[0]: " + image[0]);
         log.info("Example byte of input [0]: " + input[0]);
 
@@ -62,16 +63,20 @@ public class BitSetStego {
             for (int i = 7; i < imageBits.size(); i = i + 7) {
                 imageBits.set(i, inputBits.get(inputBitPosition));
                 inputBitPosition++;
+                if(inputBitPosition == inputBits.length()){
+                    break;
+                }
             }
 
             log.info("Example encoded bit of image [0] " + imageBits.get(0));
 
             //convert bits to Bytes[]
-            byte[] newImageBytes = imageBits.toByteArray();
-
-            log.info("Example encoded byte of image[0] " + newImageBytes[0]);
-
+            byte[] newImageBytes = new byte[header.length + imageBits.length()];
+            System.arraycopy(header, 0, newImageBytes, 0, header.length);
+            System.arraycopy(imageBits.toByteArray(), 0, newImageBytes, header.length, imageBits.toByteArray().length);
+            log.info("Modified image header " + newImageBytes[0]);
             renderOutputImage(newImageBytes);
+
         } else {
             log.warning("Image file too small to encode given input");
         }
@@ -95,5 +100,42 @@ public class BitSetStego {
 
     private static void renderOutputImage(byte[] newImageBytes) {
         //TODO
+//        log.info("newImageBytes: " + Arrays.toString(newImageBytes));
+        try {
+            ByteArrayInputStream bis = new ByteArrayInputStream(newImageBytes);
+            BufferedImage bufferedImage = ImageIO.read(bis);
+            ImageIO.write(bufferedImage, "bmp", new File("src/main/java/images/all_white_test_new.bmp"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //bytes 18-21 represent width. 22-25 represent height
+//        byte[] widthArray = Arrays.copyOfRange(newImageBytes, 18, 22);
+//        byte[] heightArray = Arrays.copyOfRange(newImageBytes, 22, 26);
+//
+//        log.info("start of array: " +  Arrays.toString(Arrays.copyOfRange(newImageBytes, 0, 27)));
+//        log.info("widthArray: " + Arrays.toString(widthArray));
+//        log.info("heightArray: " + Arrays.toString(heightArray));
+//        for ( int x = 0; x < widthArray.length; x++) {
+//            log.info(widthArray[x]);
+//        }
+//        log.info("heightArray: " + heightArray.toString());
+//        int width;
+//        int height;
+//        BufferedImage outputImage = new BufferedImage(newImageBytes.get(18), newImageBytes[22], );
+//
+//        try {
+//            ImageIO.write(outputImage, "BMP", new File("src/main/java/images/output.bmp"));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        try
+//        {
+//            File ms = new File();
+//            ms.Write(byteArrayIn, 0, byteArrayIn.Length);
+//            returnImage = Image.FromStream(ms,true);//Exception occurs here
+//        }
+//        catch { }
     }
 }
